2ee0e449053192ea8d7876ee8350fd3c759bbddf
sayu <aurantiacus930@gmail.com>
2019-10-05 10:30:57 +0800

    fix some modules

--- tf_model/incept_resnet_pytorch.py
+++ tf_model/incept_resnet_pytorch.py
@@ -24,24 +24,24 @@ class SlimConv2d(nn.Module):
 
 class SlimConv2dTranspose(nn.Module):
     def __init__(self, in_ch, out_ch, kernel_size=2, stride=2, padding=1):
-        super(SlimConv2d, self).__init__()
+        super(SlimConv2dTranspose, self).__init__()
         self.slim_conv2d_transpose = nn.Sequential(
-                nn.ConvTranpose2d(in_ch, out_ch, kernel_size, stride),
+                nn.ConvTranspose2d(in_ch, out_ch, kernel_size, stride),
                 SlimBatchNorm2d(out_ch),
                 nn.ReLU()
         )
 
     def forward(self, x):
-        return self.slim_conv_transpose(x)
+        return self.slim_conv2d_transpose(x)
 
 
 class SlimFullyConnected(nn.Module):
 
-    def __init__(self, in_ch, out_ch, kernel_size=3, stride=1, padding=1):
+    def __init__(self, in_feature, out_feature):
         super(SlimFullyConnected, self).__init__()
-        self.slim_conv = nn.Sequential(
-                nn.Linear(in_ch, out_ch),
-                nn.BatchNorm2d(out_ch),
+        self.slim_fully_connected = nn.Sequential(
+                nn.Linear(in_feature, out_feature),
+                nn.BatchNorm1d(out_feature),
                 nn.ReLU()
         )
 
@@ -51,10 +51,10 @@ class SlimFullyConnected(nn.Module):
 
 class ConvMaxpool(nn.Module):
     """ simple conv + max_pool """
-    def __init__(self):
+    def __init__(self, in_ch, out_ch, kernel_size=3, stride=1, padding=1):
         super(ConvMaxpool, self).__init__()
-        self.conv = SlimConv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1)
-        self.max_pool2d = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv = SlimConv2d(in_ch, out_ch, kernel_size, stride, padding)
+        self.max_pool2d = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
     def forward(self, x):
         return self.max_pool2d(self.conv(x)) 
